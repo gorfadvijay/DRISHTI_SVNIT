9aa936ce3446a60703841dcb44b9b390b6e89ba5
Gorfad_Vijay <gorfadvijay1234@gmail.com>
2018-12-18 01:40:20 +0530

    Improved Footer Bootstrap And Social Icon footer added

--- footer.php
+++ footer.php
@@ -1,29 +1,162 @@
-<footer class="footer container-fluid">
-    <div class="container"> 
-  <div class="space-10"></div>
-  <div class="row" >
-    <div class="col-md-4 margin-b-30 logos" style="">
-      <div class="margin-b-20"></div>
-        <a href="#">
-          <h2 style="color: #ffffff;">DRISHTI</h2>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<div class="container-fluid"style="max-width: ">
+      
+  <div class="row" style="background-color:#202020;  ">
+    <div class="col-sm-4 margin-b-30 text-center" style="margin-top: 40px;" >
+     <a href="#">
+          <h2 style="color: #ffffff; margin: 20px;">DRISHTI</h2>
         </a>
-      <p>
+      <h4 style="color: #ffffff;  margin: 20px;">
         We are the Technical Club of NIT SURAT
-      </p>
+      </h4>
     </div>
-    <div class="col-md-4 contact" style="">
-      <ul class="list-unstyled" style="height: auto">
-        <li><a href="#">Contact Us</a></li>
-        <ul class="list-inline social">
-          <li><a href="#"><i class="ion-social-facebook"></i></a></li>
-          <li><a href="#"><i class="ion-social-googleplus"></i></a></li>
-          <li><a href="#"><i class="ion-social-youtube"></i></a></li>
-        </ul>
-      </ul>
+    
+    <div class="col-sm-4">
+      <!DOCTYPE html>
+  <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/all.js" integrity="sha384-xymdQtn1n3lH2wcu0qhcdaOpQwyoarkgLVxC/wZ5q7h9gHtxICrpcaSUfygqZGOe" crossorigin="anonymous"></script>
+
+
+  <div class="rounded-social-buttons" style="margin: 50px;">
+                  <div style="font-size: 26px;margin-top: 20px;
+                  margin-bottom: 20px;  color: #f2f2f2">Follow Us  
+                  </div>
+
+                    <a class="social-button facebook" href="https://www.facebook.com/" target="_blank"><i class="fab fa-facebook-f"></i></a>
+                    <a class="social-button twitter" href="https://www.twitter.com/" target="_blank"><i class="fab fa-twitter"></i></a>
+                    <a class="social-button linkedin" href="https://www.linkedin.com/" target="_blank"><i class="fab fa-linkedin"></i></a>
+                    <a class="social-button youtube" href="https://www.youtube.com/" target="_blank"><i class="fab fa-youtube"></i></a>
+                    <a class="social-button instagram" href="https://www.instagram.com/" target="_blank"><i class="fab fa-instagram"></i></a>
+  </div>
+
+  
+  
+
+<style type="text/css">
+
+textarea {
+    resize: none;
+}
+
+.text {
+  color: white;
+  font-size: 20px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  -ms-transform: translate(-50%, -50%);
+  white-space: nowrap;
+}
+
+@charset "UTF-8";
+
+.svg-inline--fa {
+  vertical-align: -0.200em;
+}
+
+.rounded-social-buttons {
+  text-align: center;
+}
+
+.rounded-social-buttons .social-button {
+  display: inline-block;
+  position: relative;
+  cursor: pointer;
+  width: 3.125rem;
+  height: 3.125rem;
+  border: 0.125rem solid transparent;
+  padding: 0;
+  text-decoration: none;
+  text-align: center;
+  color: #fefefe;
+  font-size: 1.5625rem;
+  font-weight: normal;
+  line-height: 2em;
+  border-radius: 1.6875rem;
+  transition: all 0.5s ease;
+  margin-right: 0.25rem;
+  margin-bottom: 0.25rem;
+}
+
+.rounded-social-buttons .social-button:hover, .rounded-social-buttons .social-button:focus {
+  -webkit-transform: rotate(360deg);
+      -ms-transform: rotate(360deg);
+          transform: rotate(360deg);
+}
+
+.rounded-social-buttons .fa-twitter, .fa-facebook-f, .fa-linkedin, .fa-youtube, .fa-instagram {
+  font-size: 25px;
+}
+
+.rounded-social-buttons .social-button.facebook {
+  background: #3b5998;
+}
+
+.rounded-social-buttons .social-button.facebook:hover, .rounded-social-buttons .social-button.facebook:focus {
+  color: #3b5998;
+  background: #fefefe;
+  border-color: #3b5998;
+}
+
+.rounded-social-buttons .social-button.twitter {
+  background: #55acee;
+}
+
+.rounded-social-buttons .social-button.twitter:hover, .rounded-social-buttons .social-button.twitter:focus {
+  color: #55acee;
+  background: #fefefe;
+  border-color: #55acee;
+}
+
+.rounded-social-buttons .social-button.linkedin {
+  background: #007bb5;
+}
+
+.rounded-social-buttons .social-button.linkedin:hover, .rounded-social-buttons .social-button.linkedin:focus {
+  color: #007bb5;
+  background: #fefefe;
+  border-color: #007bb5;
+}
+
+.rounded-social-buttons .social-button.youtube {
+  background: #bb0000;
+}
+
+.rounded-social-buttons .social-button.youtube:hover, .rounded-social-buttons .social-button.youtube:focus {
+  color: #bb0000;
+  background: #fefefe;
+  border-color: #bb0000;
+}
+
+.rounded-social-buttons .social-button.instagram {
+  background: #cd486b;
+}
+
+.rounded-social-buttons .social-button.instagram:hover, .rounded-social-buttons .social-button.instagram:focus {
+  color: #cd486b;
+  background: #fefefe;
+  border-color: #cd486b;
+}</style>
+
+
+
+
+    <div class="col-sm-12 text-center" style="color: #f1f1f0; margin: 20px; font-size: 18px;" >
+       &copy; 2018 Drishti-SVNIT. All Rights Reserved
     </div>
-    <div class="col-md-4 copyrit" style="">&copy;2017 SVNIT. All rights reserved</div>
   </div>
 </div>
-</footer>
-
-	
+    
+</body>
+</html>
