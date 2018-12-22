d347783e71162d55cdf54a2e044524e8a1ad8165
Gorfad_Vijay <gorfadvijay1234@gmail.com>
2018-12-22 15:05:59 +0530

    Added contactus php mysql Data base page

---
+++ admin/contactus.php
@@ -0,0 +1,50 @@
+<?php 
+include 'connect.php';
+$sql = "SELECT * FROM `contact";
+$result = mysqli_query($dbconn, $sql);
+
+if(isset($_POST['name'])){
+    $name=$_POST['name'];
+};
+if(isset($_POST['email'])){
+    $email=$_POST['email'];
+};
+if(isset($_POST['msg'])){
+    $msg=$_POST['msg'];
+};
+$id="";
+
+
+
+
+
+$insert_sql = "INSERT INTO 
+contact (id,name,email,msg)
+
+VALUES ('$id','$name','$email','$msg')";
+
+if (mysqli_query($dbconn, $insert_sql)) {
+  ?>
+ <script language="JavaScript" type="text/javascript">
+            alert ("Thanks For your Message!!!!");
+            window.location.assign("../contact.php");
+            </script>
+           <?php 
+
+} else {
+   ?>
+ <script language="JavaScript" type="text/javascript">
+            alert ("Sorry Server Is down Please contact Devloper");
+            window.location.assign("../contact.php");
+            </script>
+           <?php 
+
+}
+mysqli_close($dbconn);
+
+//header("Location:competition_insert_form.html");
+
+
+
+
+ ?>
